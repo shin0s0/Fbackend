869eb8c241c16f9dc5dbd74c7c1b8329ebf97b00
arpan <arpanchand3@gmail.com>
2024-09-15 15:58:39 +0530

    fix

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ app.use('/api/auth', authRoutes);
 app.use('/api/friends', friendRoutes);
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/mernfriendapp', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
-
+  
 // Start Server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
