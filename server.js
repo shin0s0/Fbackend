e7ea9435ea0c01fc5462aee0e8ef81735371ab23
arpan <arpanchand3@gmail.com>
2024-09-15 16:16:20 +0530

    fix

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
 app.use(bodyParser.json());
 
 // Routes
